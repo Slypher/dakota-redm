7dc2f6e94bc56807531c0f59c697ae31af45e884
slashkeyvalue <felipekvalue@gmail.com>
2021-10-18 22:42:50 -0300

    tweak(core/Chests): adiciona mais baús.

--- resources/[FAROESTE]/_core/config/Chests.lua
+++ resources/[FAROESTE]/_core/config/Chests.lua
@@ -20,9 +20,26 @@ config_file_STATICCHESTS = {
     {"static:public:1", -327.972, 775.927, 121.639 - 0.89, 10.0, 1, 100},
     {"static:public:2", -324.945, 758.769, 121.639 - 0.89, 185.00, 1, 100},
     {"static:public:3", -328.118, 764.176, 117.439 - 0.89, 100.00, 1, 100},
+
+    {"static:public:6", -302.983, 764.042, 118.643 - 0.89, -77.00, 0, 100}, -- BANQUEIRO VALENTINE
+    {"static:public:7", -820.725, -1274.807, 43.586 - 0.89, -176.0, 0, 100}, -- BANQUEIRO BLACKWATER
+    {"static:public:8", 1287.052, -1311.952, 76.979 - 0.89, -36.00, 0, 100}, -- BANQUEIRO RHODES
     -- ///////// RHODES
     {"static:public:4", 1351.093, -1380.077, 84.287 - 0.89, 259.999, 1, 100},
+    
     {"static:trooper:1", 2506.955, -1301.881, 48.954 - 0.89, 49.59, 0, 100, "trooper"}, -- saint dennis
     {"static:trooper:2", 1361.919, -1305.239, 77.761 - 0.89, 246.999, 0, 100, "trooper"}, --- rhodes
-    {"static:trooper:3", -279.592, 808.447, 119.380 - 0.89, 93.51, 0, 100, "trooper"} -- VALENTINE
+    {"static:trooper:3", -279.592, 808.447, 119.380 - 0.89, 93.51, 0, 100, "trooper"}, -- VALENTINE
+
+    -- #    INDIOS
+    {"static:indio:1", 467.791,2228.419,247.358 - 0.89, 100.00, 1, 100, "indio"}, -- RESERVA
+
+    -- #    FERREIRO
+    {"static:ferreiro:1", -363.782,795.574,116.135 - 0.89, -170.00, 1, 100, "ferreiro"}, -- VALENTINE
+
+    -- #    ARMEIRO
+    {"static:armeiro:1", -283.657,778.408,119.443 - 0.89, 90.0, 1, 100, "armeiro"}, -- VALENTINE
+
+    -- #    DOCTOR
+    {"static:medic:1", -288.720,812.972,119.325 - 0.89, 8.00, 0, 100, "medic"} -- VALENTINE
 }
