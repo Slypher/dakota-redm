97933d928c8f37966f6b98d66a96e6af8b9c967a
slashkeyvalue <felipekvalue@gmail.com>
2021-11-05 22:51:32 -0300

    feat: newspaper.

--- resources/[FAROESTE]/[COMPONENTES]/frp_newspaper/fxmanifest.lua
+++ resources/[FAROESTE]/[COMPONENTES]/frp_newspaper/fxmanifest.lua
@@ -2,14 +2,22 @@ fx_version 'adamant'
 games {'rdr3'}
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
-server_script("@_core/lib/utils.lua")
-server_script('server.lua')
+server_scripts {
+    "@_core/lib/utils.lua",
+    "server.lua"
+}
 
-client_script('client/client.lua')
+client_scripts {
+    'client/client.lua'
+}
 
 ui_page('html/index.html')
 
 files({
-    'html/*',
+    'html/**/*',
     'server.lua'
 })
+
+
+this_is_a_map "yes"
+
---
+++ resources/[FAROESTE]/[COMPONENTES]/frp_newspaper/html/editions/editions.js
@@ -0,0 +1,17 @@
+var editions = [
+    {
+        metadata: "ed01",
+        name: "Edição 1º",
+        pages: [
+            {
+                image: "ed1/1.png"
+            },
+            {
+                image: "ed1/2.png"
+            },
+            {
+                image: "ed1/3.png"
+            },
+        ]
+    }
+]
