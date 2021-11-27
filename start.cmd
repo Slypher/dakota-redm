a553f402bd0280901a42fbafee45b98f76e4944b
slashkeyvalue <felipekvalue@gmail.com>
2021-11-26 21:32:29 -0300

    tweak(start.cmd): Blood Money DLC.

--- start.cmd
+++ start.cmd
@@ -5,5 +5,5 @@ echo     Base DakotaRP
 echo     Discord:discord.gg/dakotarp 
 echo ===-------------------------------===
 
-start ..\FXServer.exe +exec configs/server.cfg +set onesync on  +set sv_enforceGameBuild 1355 +set gamename rdr3 +set svgui_disable true
+start ..\FXServer.exe +exec configs/server.cfg +set onesync on  +set sv_enforceGameBuild 1436 +set gamename rdr3 +set svgui_disable true
 exit
