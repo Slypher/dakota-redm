d6a8e5c1154892438fec9117dd9b88cb97fbf972
Lucas Lima Souza <lucas39624@gmail.com>
2021-12-07 13:33:50 -0300

    Remove add_money

--- resources/add_money/fxmanifest.lua
+++
@@ -1,8 +0,0 @@
-fx_version 'cerulean'
-game 'rdr3'
-rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
-
-server_scripts {
-    '@_core/lib/utils.lua',
-    'server.lua'
-}
